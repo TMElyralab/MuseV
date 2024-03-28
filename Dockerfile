7766c014baddec606a921a34084426360b305a0f
anchorxia <anchorxia@tencent.com>
2024-03-28 23:46:11 +0800

    [Add](docker): add huggingface gradio docker update musev docker from musev:1.0.0 instead of pytorch docker

--- Dockerfile
+++ Dockerfile
@@ -1,49 +1,19 @@
-FROM pytorch/pytorch:2.0.1-cuda11.7-cudnn8-devel
+FROM anchorxia/musev:1.0.0
 
 #MAINTAINER 维护者信息
 LABEL MAINTAINER="anchorxia"
 LABEL Email="anchorxia@tencent.com"
-LABEL Description="gpu development image, from docker pull pytorch/pytorch:2.0.1-cuda11.7-cudnn8-devel"
+LABEL Description="musev gpu runtime image, base docker is pytorch/pytorch:2.0.1-cuda11.7-cudnn8-devel"
 ARG DEBIAN_FRONTEND=noninteractive
 
 USER root
-# 安装必须软件
-RUN apt -y update \
-    && apt -y upgrade
-RUN apt install -y wget git curl tmux cmake htop iotop git-lfs zip \
-    && apt install -y autojump \
-    && apt install -y libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg \
-    && apt clean
 
 SHELL ["/bin/bash", "--login", "-c"]
 
-RUN conda create -n musev python=3.10.6 -y \
-    && . /opt/conda/etc/profile.d/conda.sh  \
+RUN . /opt/conda/etc/profile.d/conda.sh  \
     && echo "source activate musev" >> ~/.bashrc \
     && conda activate musev \
-    && pip install tensorflow==2.12.0 tensorboard==2.12.0 \
-    # && pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html -i https://mirrors.bfsu.edu.cn/pypi/web/simple -U \
-    && pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118 \
-    && pip install ninja==1.11.1 \
-    && pip install --no-cache-dir transformers==4.33.1 bitsandbytes==0.41.1 decord==0.6.0 accelerate==0.22.0 xformers==0.0.21 omegaconf einops imageio==2.31.1 \
-    && pip install --no-cache-dir pandas h5py matplotlib modelcards==0.1.6 pynvml==11.5.0 black pytest moviepy==1.0.3 torch-tb-profiler==0.4.1 scikit-learn librosa ffmpeg easydict webp mediapipe==0.10.3 \
-    && pip install --no-cache-dir cython==3.0.2 easydict gdown infomap==2.7.1 insightface==0.7.3 ipython librosa==0.10.1 onnx==1.14.1 onnxruntime==1.15.1 onnxsim==0.4.33 opencv_python Pillow protobuf==3.20.3 pytube==15.0.0 PyYAML \
-    && pip install --no-cache-dir requests scipy six tqdm gradio==3.43.2 cuid albumentations==1.3.1 opencv-contrib-python==4.8.0.76 imageio-ffmpeg==0.4.8 pytorch-lightning==2.0.8 test-tube==0.7.5 \
-    && pip install --no-cache-dir timm addict yapf prettytable safetensors==0.3.3 basicsr fvcore pycocotools wandb==0.15.10 wget ffmpeg-python \
-    && pip install --no-cache-dir streamlit webdataset kornia==0.7.0 open_clip_torch==2.20.0 streamlit-drawable-canvas==0.9.3 torchmetrics==1.1.1 \
-    # 安装暗水印
-    && pip install --no-cache-dir invisible-watermark==0.1.5 gdown==4.5.3 ftfy==6.1.1 modelcards==0.1.6 \
-    # jupyters
-    && pip install ipywidgets==8.0.3 \
-    && python -m ipykernel install --user --name projectv --display-name "python(projectv)" \
-    && pip install --no-cache-dir matplotlib==3.6.2 redis==4.5.1  pydantic[dotenv]==1.10.2 loguru==0.6.0 IProgress==0.4 \
-    && pip install git+https://github.com/tencent-ailab/IP-Adapter.git \
-    && pip install -U openmim \
-    && mim install mmengine \
-    && mim install "mmcv>=2.0.1" \
-    && mim install "mmdet>=3.1.0" \
-    && mim install "mmpose>=1.1.0" \
-    # 必须放在最后pip，避免和jupyter的不兼容
-    && pip install --no-cache-dir  markupsafe==2.0.1\
+    && conda env list \
+    && pip install cuid
 
 USER root
--- scripts/gradio/app.py
+++ scripts/gradio/app.py
@@ -3,8 +3,8 @@ import gradio as gr
 import pdb
 import cuid
 
-from gradio_videocreation_text2video import online_t2v_inference
-from gradio_videocreation_video2video import online_v2v_inference
+from gradio_text2video import online_t2v_inference
+from gradio_video2video import online_v2v_inference
 from huggingface_hub import snapshot_download
 
 
@@ -13,20 +13,23 @@ def download_model():
         print("Checkpoint Not Downloaded, start downloading...")
         snapshot_download(
             repo_id="TMElyralab/MuseV",
-            local_dir = "../../checkpoints",
+            local_dir="../../checkpoints",
             max_workers=8,
         )
     else:
         print("Already download the model.")
-download_model()# for huggingface deployment.
+
+
+download_model()  # for huggingface deployment.
 
 
 def update_shape(image):
-    if image!=None:
-        h,w,_=image.shape
+    if image != None:
+        h, w, _ = image.shape
     else:
-        h,w=768,512
-    return w,h
+        h, w = 768, 512
+    return w, h
+
 
 class ConcatenateBlock(gr.blocks.Block):
     def __init__(self, options):
@@ -41,10 +44,39 @@ class ConcatenateBlock(gr.blocks.Block):
                 self.current_string += ", " + new_choice
         return self.current_string
 
+
 def process_input(new_choice):
     return concatenate_block.update_string(new_choice), ""
 
-control_options = ['pose', 'pose_body', 'pose_hand', 'pose_face', 'pose_hand_body', 'pose_hand_face', 'dwpose', 'dwpose_face', 'dwpose_hand', 'dwpose_body', 'dwpose_body_hand', 'canny', 'tile', 'hed', 'hed_scribble', 'depth', 'pidi', 'normal_bae', 'lineart', 'lineart_anime', 'zoe', 'sam', 'mobile_sam', 'leres', 'content', 'face_detector']
+
+control_options = [
+    "pose",
+    "pose_body",
+    "pose_hand",
+    "pose_face",
+    "pose_hand_body",
+    "pose_hand_face",
+    "dwpose",
+    "dwpose_face",
+    "dwpose_hand",
+    "dwpose_body",
+    "dwpose_body_hand",
+    "canny",
+    "tile",
+    "hed",
+    "hed_scribble",
+    "depth",
+    "pidi",
+    "normal_bae",
+    "lineart",
+    "lineart_anime",
+    "zoe",
+    "sam",
+    "mobile_sam",
+    "leres",
+    "content",
+    "face_detector",
+]
 concatenate_block = ConcatenateBlock(control_options)
 
 
@@ -52,7 +84,8 @@ css = """#input_img {max-width: 1024px !important} #output_vid {max-width: 1024p
 
 
 with gr.Blocks(css=css) as demo:
-    gr.Markdown("<div align='center'> <h1> MuseV: Infinite-length and High Fidelity Virtual Human Video Generation with Visual Conditioned Parallel Denoising</span> </h1> \
+    gr.Markdown(
+        "<div align='center'> <h1> MuseV: Infinite-length and High Fidelity Virtual Human Video Generation with Visual Conditioned Parallel Denoising</span> </h1> \
                     <h2 style='font-weight: 450; font-size: 1rem; margin: 0rem'>\
                     </br>\
                     Zhiqiang Xia <sup>*</sup>,\
@@ -71,56 +104,75 @@ with gr.Blocks(css=css) as demo:
                 <a style='font-size:18px;color: #000000'>, which is important to Open-Source projects. Thanks!</a>\
                 <a style='font-size:18px;color: #000000' href=''> [ArXiv(Coming Soon)] </a>\
                 <a style='font-size:18px;color: #000000' href=''> [Project Page(Coming Soon)] </a> \
-                <a style='font-size:18px;color: #000000'>If MuseV is useful, please help star the repo~ </a> </div>" )
+                <a style='font-size:18px;color: #000000'>If MuseV is useful, please help star the repo~ </a> </div>"
+    )
     with gr.Tab("Text to Video"):
         with gr.Row():
             with gr.Column():
-                prompt=gr.Textbox(label='Prompt')
-                image=gr.Image(label="Reference Image")
-                seed=gr.Number(label='Seed')
-                video_length=gr.Number(label='Video Length',value=12)
-                fps=gr.Number(label='Generate Video FPS',value=12)
+                prompt = gr.Textbox(label="Prompt")
+                image = gr.Image(label="Reference Image")
+                seed = gr.Number(label="Seed")
+                video_length = gr.Number(label="Video Length", value=12)
+                fps = gr.Number(label="Generate Video FPS", value=12)
                 gr.Markdown("Default use the Reference Image's Size.")
                 with gr.Row():
-                    w=gr.Number(label='Width',value=512)
-                    h=gr.Number(label='Height',value=768)
+                    w = gr.Number(label="Width", value=512)
+                    h = gr.Number(label="Height", value=768)
                 btn1 = gr.Button("Generate")
             out = gr.outputs.Video()
             # pdb.set_trace()
         with gr.Row():
-            board = gr.Dataframe(value=[["", "", ""]] * 3, interactive=False, type="array",label='Demo Video')
-            
-        image.change(fn=update_shape,inputs=[image],outputs=[w,h])
-        
-        btn1.click(fn=online_t2v_inference, inputs=[prompt,image,seed,fps,w,h,video_length], outputs=out)
+            board = gr.Dataframe(
+                value=[["", "", ""]] * 3,
+                interactive=False,
+                type="array",
+                label="Demo Video",
+            )
+
+        image.change(fn=update_shape, inputs=[image], outputs=[w, h])
+
+        btn1.click(
+            fn=online_t2v_inference,
+            inputs=[prompt, image, seed, fps, w, h, video_length],
+            outputs=out,
+        )
 
     with gr.Tab("Video to Video"):
         with gr.Row():
             with gr.Column():
-                prompt=gr.Textbox(label='Prompt')
-                image=gr.Image(label="Reference Image")
-                video=gr.Video(label="Input Video")
+                prompt = gr.Textbox(label="Prompt")
+                image = gr.Image(label="Reference Image")
+                video = gr.Video(label="Input Video")
                 # radio = gr.inputs.Radio(, label="Select an option")
                 # ctr_button = gr.inputs.Button(label="Add ControlNet List")
                 # output_text = gr.outputs.Textbox()
-                processor=gr.Textbox(label=f'Control Condition. Select from {control_options}', value='dwpose,')
-                seed=gr.Number(label='Seed')
-                video_length=gr.Number(label='Video Length',value=12)
-                fps=gr.Number(label='Generate Video FPS',value=12)
+                processor = gr.Textbox(
+                    label=f"Control Condition. Select from {control_options}",
+                    value="dwpose,",
+                )
+                seed = gr.Number(label="Seed")
+                video_length = gr.Number(label="Video Length", value=12)
+                fps = gr.Number(label="Generate Video FPS", value=12)
                 gr.Markdown("If W&H is None, then use the Reference Image's Size")
                 with gr.Row():
-                    w=gr.Number(label='Width',value=512)
-                    h=gr.Number(label='Height',value=704)
+                    w = gr.Number(label="Width", value=512)
+                    h = gr.Number(label="Height", value=704)
                 btn2 = gr.Button("Generate")
             out1 = gr.outputs.Video()
-        image.change(fn=update_shape,inputs=[image],outputs=[w,h])
+        image.change(fn=update_shape, inputs=[image], outputs=[w, h])
 
-        btn2.click(fn=online_v2v_inference, inputs=[prompt,image,video,processor,seed,fps,w,h,video_length], outputs=out1)
+        btn2.click(
+            fn=online_v2v_inference,
+            inputs=[prompt, image, video, processor, seed, fps, w, h, video_length],
+            outputs=out1,
+        )
 
 
 # Set the IP and port
 ip_address = "0.0.0.0"  # Replace with your desired IP address
-port_number = 7860      # Replace with your desired port number
+port_number = 7860  # Replace with your desired port number
 
 
-demo.queue().launch(share=False, debug=False, server_name=ip_address, server_port=port_number)
+demo.queue().launch(
+    share=False, debug=False, server_name=ip_address, server_port=port_number
+)
