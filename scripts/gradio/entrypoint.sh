3115cf2c2d72a55a572ff29e3d140b219106ff16
anchorxia <anchorxia@tencent.com>
2024-03-30 20:22:42 +0800

    update dockerfile

---
+++ scripts/gradio/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "entrypoint.sh"
+whoami
+which python
+export PYTHONPATH=${PYTHONPATH}:/home/user/app/MuseV:/home/user/app/MuseV/MMCM:/home/user/app/MuseV/diffusers/src:/home/user/app/MuseV/controlnet_aux/src
+echo "pythonpath" $PYTHONPATH
+# chmod 777 -R /home/user/app/MuseV
+# Print the contents of the diffusers/src directory
+# echo "Contents of /home/user/app/MuseV/diffusers/src:"
+# Load ~/.bashrc
+# source ~/.bashrc
+
+source /opt/conda/etc/profile.d/conda.sh
+conda activate musev
+which python
+python app.py
