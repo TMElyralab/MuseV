b332ae4d3720d62f670ac27ef6c6a7654d11ac95
zhanchao019 <trumpzhan@tencent.com>
2024-04-03 14:38:56 +0800

    reshape logci

--- scripts/gradio/app_space.py
+++ scripts/gradio/app_space.py
@@ -13,8 +13,6 @@ ProjectDir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 CheckpointsDir = os.path.join(ProjectDir, "checkpoints")
 
 
-
-
 def download_model():
     if not os.path.exists(CheckpointsDir):
         print("Checkpoint Not Downloaded, start downloading...")
@@ -35,6 +33,8 @@ download_model()  # for huggingface deployment.
 from gradio_video2video import online_v2v_inference
 from gradio_text2video import online_t2v_inference
 
+
+
 @spaces.GPU(duration=180)
 def hf_online_t2v_inference(
     prompt,
@@ -46,10 +46,12 @@ def hf_online_t2v_inference(
     video_len,
     img_edge_ratio,
 ):
-    return online_t2v_inference(prompt,image_np,seed,fps,w,h,video_len,img_edge_ratio)
+    return online_t2v_inference(
+        prompt, image_np, seed, fps, w, h, video_len, img_edge_ratio
+    )
+
 
 @spaces.GPU(duration=180)
-
 def hg_online_v2v_inference(
     prompt,
     image_np,
@@ -59,9 +61,21 @@ def hg_online_v2v_inference(
     fps,
     w,
     h,
-    video_length,img_edge_ratio,
+    video_length,
+    img_edge_ratio,
 ):
-    return online_v2v_inference(prompt,image_np,video,processor,seed,fps,w,h,video_length,img_edge_ratio)
+    return online_v2v_inference(
+        prompt,
+        image_np,
+        video,
+        processor,
+        seed,
+        fps,
+        w,
+        h,
+        video_length,
+        img_edge_ratio,
+    )
 
 
 def update_shape(image):
@@ -69,35 +83,31 @@ def update_shape(image):
         h, w, _ = image.shape
     else:
         h, w = 768, 512
-    return w, h,w,h
+    return w, h, w, h
 
-def limit_shape(image,input_w,input_h,img_edge_ratio):
-    
-    if isinstance(image, np.ndarray):
-        h, w, _ = image.shape
-    else:
-        h, w = -1, -1
-    if input_h!=-1:
-        h=input_h
-    if input_w!=-1:
-        w=input_w
-        
-    h=img_edge_ratio*h
-    w=img_edge_ratio*w
 
-    max_dim = 960
-    if w > max_dim or h > max_dim:
-        scale_ratio = min(max_dim / w, max_dim / h)
-        new_w = int(w * scale_ratio)
-        new_h = int(h * scale_ratio)
-    else:
-        new_w=w
-        new_h=h
-    return new_w, new_h
+def limit_shape(image, input_w, input_h, img_edge_ratio, max_image_edge=960):
+    """limite generation video shape to avoid gpu memory overflow"""
+    if isinstance(image, np.ndarray) and (input_h == -1 and input_w == -1):
+        input_h, input_w, _ = image.shape
+
+    img_edge_ratio_infact = min(max_image_edge / max(input_h, input_w), img_edge_ratio)
+    # print(
+    #     image.shape,
+    #     input_w,
+    #     input_h,
+    #     img_edge_ratio,
+    #     max_image_edge,
+    #     img_edge_ratio_infact,
+    # )
+    return img_edge_ratio_infact
+
 
 def limit_length(length):
-    if length>24*6:
-        length=24*6
+    """limite generation video frames numer to avoid gpu memory overflow"""
+
+    if length > 24 * 6:
+        length = 24 * 6
     return length
 
 
@@ -181,8 +191,14 @@ with gr.Blocks(css=css) as demo:
             with gr.Column():
                 prompt = gr.Textbox(label="Prompt")
                 image = gr.Image(label="VisionCondImage")
-                seed = gr.Number(label="Seed (seed=-1 means that the seeds run each time are different)", value=-1)
-                video_length = gr.Number(label="Video Length(need smaller than 144,If you want to be able to generate longer videos, run it locally )", value=12)
+                seed = gr.Number(
+                    label="Seed (seed=-1 means that the seeds run each time are different)",
+                    value=-1,
+                )
+                video_length = gr.Number(
+                    label="Video Length(need smaller than 144,If you want to be able to generate longer videos, run it locally )",
+                    value=12,
+                )
                 fps = gr.Number(label="Generate Video FPS", value=6)
                 gr.Markdown(
                     (
@@ -196,11 +212,11 @@ with gr.Blocks(css=css) as demo:
                     w = gr.Number(label="Width", value=-1)
                     h = gr.Number(label="Height", value=-1)
                     img_edge_ratio = gr.Number(label="img_edge_ratio", value=1.0)
-                with gr.Row():
-                    final_w = gr.Number(label="Generate Width", value=-1)
-                    final_h = gr.Number(label="Generate Height", value=-1)
+                    img_edge_ratio_infact = gr.Number(
+                        label="img_edge_ratio in fact", value=1.0
+                    )
                 btn1 = gr.Button("Generate")
-            out = gr.outputs.Video()
+            out = gr.Video()
             # pdb.set_trace()
         # with gr.Row():
         #     board = gr.Dataframe(
@@ -209,18 +225,28 @@ with gr.Blocks(css=css) as demo:
         #         type="array",
         #         label="Demo Video",
         #     )
+        img_edge_ratio.change(
+            fn=limit_shape,
+            inputs=[image, w, h, img_edge_ratio],
+            outputs=[img_edge_ratio_infact],
+        )
 
-        image.change(fn=update_shape, inputs=[image], outputs=[w, h,final_w,final_h])
-
-        w.change(fn=limit_shape,inputs=[image,w,h,img_edge_ratio],outputs=[final_w,final_h])
-        h.change(fn=limit_shape,inputs=[image,w,h,img_edge_ratio],outputs=[final_w,final_h])
-        img_edge_ratio.change(fn=limit_shape,inputs=[image,w,h,img_edge_ratio],outputs=[final_w,final_h])
-        
-        video_length.change(fn=limit_length,inputs=[video_length],outputs=[video_length])
+        video_length.change(
+            fn=limit_length, inputs=[video_length], outputs=[video_length]
+        )
 
         btn1.click(
             fn=hf_online_t2v_inference,
-            inputs=[prompt, image, seed, fps, w, h, video_length, img_edge_ratio],
+            inputs=[
+                prompt,
+                image,
+                seed,
+                fps,
+                w,
+                h,
+                video_length,
+                img_edge_ratio_infact,
+            ],
             outputs=out,
         )
 
@@ -244,7 +270,10 @@ with gr.Blocks(css=css) as demo:
                     value="dwpose_body_hand",
                 )
                 gr.Markdown("seed=-1 means that seeds are different in every run")
-                seed = gr.Number(label="Seed (seed=-1 means that the seeds run each time are different)", value=-1)
+                seed = gr.Number(
+                    label="Seed (seed=-1 means that the seeds run each time are different)",
+                    value=-1,
+                )
                 video_length = gr.Number(label="Video Length", value=12)
                 fps = gr.Number(label="Generate Video FPS", value=6)
                 gr.Markdown(
@@ -259,17 +288,19 @@ with gr.Blocks(css=css) as demo:
                     w = gr.Number(label="Width", value=-1)
                     h = gr.Number(label="Height", value=-1)
                     img_edge_ratio = gr.Number(label="img_edge_ratio", value=1.0)
-                with gr.Row():
-                    final_w = gr.Number(label="Generate Width", value=-1)
-                    final_h = gr.Number(label="Generate Height", value=-1)
+                    img_edge_ratio_infact = gr.Number(
+                        label="img_edge_ratio in fact", value=1.0
+                    )
                 btn2 = gr.Button("Generate")
-            out1 = gr.outputs.Video()
-        image.change(fn=update_shape, inputs=[image], outputs=[w, h])
-        final_w.change(fn=limit_shape,inputs=[image,img_edge_ratio],outputs=[final_w,final_h])
-        final_h.change(fn=limit_shape,inputs=[image,img_edge_ratio],outputs=[final_w,final_h])
-        
-        video_length.change(fn=limit_length,inputs=[video_length],outputs=[video_length])
-        
+            out1 = gr.Video()
+        img_edge_ratio.change(
+            fn=limit_shape,
+            inputs=[image, w, h, img_edge_ratio],
+            outputs=[img_edge_ratio_infact],
+        )
+        video_length.change(
+            fn=limit_length, inputs=[video_length], outputs=[video_length]
+        )
         btn2.click(
             fn=hg_online_v2v_inference,
             inputs=[
@@ -282,7 +313,7 @@ with gr.Blocks(css=css) as demo:
                 w,
                 h,
                 video_length,
-                img_edge_ratio,
+                img_edge_ratio_infact,
             ],
             outputs=out1,
         )
@@ -294,5 +325,5 @@ port_number = 7860  # Replace with your desired port number
 
 
 demo.queue().launch(
-    share=False, debug=True, server_name=ip_address, server_port=port_number
+    share=False , debug=False, server_name=ip_address, server_port=port_number
 )
