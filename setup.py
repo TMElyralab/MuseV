80598e18f733ff857b65350fe792606d1dea321b
xzqjack <xzqjack@hotmail.com>
2024-04-11 17:36:59 +0800

    uppdate gradio space files

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
+import subprocess
+import os
+import pkg_resources
 
 from setuptools import setup, find_packages
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+ProjectDir = os.path.dirname(__file__)
+result = subprocess.run(["pip", "install", "basicsr"], capture_output=True, text=True)
 
-with open("requirements.txt", "r") as f:
+
+with open(os.path.join(ProjectDir, "requirements.txt"), "r") as f:
     requirements = f.read().splitlines()
 requirements = [x for x in requirements if x and not x.startswith("#")]
 requirements = [x.split(" ")[0] if "index-url" in x else x for x in requirements]
@@ -18,17 +22,32 @@ setup(
     description="Package about human video creation",
     # long_description=long_description,
     # long_description_content_type="text/markdown",
+    url="https://github.com/TMElyralab/MuseV",
     packages=find_packages("musev"),
     package_dir={"": "musev"},
-    url="https://github.com/TMElyralab/MuseV",
     # include_package_data=True, # please edit MANIFEST.in
-    # packages=find_packages(),  # used in import
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    # extras_require=extras_require,
     install_requires=requirements,
-    dependency_links=["https://download.pytorch.org/whl/cu118"],
+    # dependency_links=["https://download.pytorch.org/whl/cu118],
+)
+
+
+result = subprocess.run(
+    ["pip", "install", "--no-cache-dir", "-U", "openmim"],
+    capture_output=True,
+    text=True,
+)
+result = subprocess.run(["mim", "install", "mmengine"], capture_output=True, text=True)
+result = subprocess.run(
+    ["mim", "install", "mmcv>=2.0.1"], capture_output=True, text=True
+)
+result = subprocess.run(
+    ["mim", "install", "mmdet>=3.1.0"], capture_output=True, text=True
+)
+result = subprocess.run(
+    ["mim", "install", "mmpose>=1.1.0"], capture_output=True, text=True
 )
